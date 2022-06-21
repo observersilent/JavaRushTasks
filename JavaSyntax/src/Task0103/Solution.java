562c40bce0ca8e9bae4a9b1a96c39e6edf9f6914
observersilent <denek888@mail.ru>
2022-06-21 21:09:06 +0500

    Добавление задания

---
+++ JavaSyntax/src/Task0103/Solution.java
@@ -0,0 +1,9 @@
+//Сейчас 3126 год. Мой друг родился 8 лет назад. Вывести на экран день рождения моего друга
+
+package Task0103;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(3126 - 8);
+    }
+}
