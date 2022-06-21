42e08fb5d4e530daf79b08048fb97478b0e962c3
observersilent <denek888@mail.ru>
2022-06-21 19:42:27 +0500

    Первое задание

--- JavaSyntax/src/task0101.java
+++ JavaSyntax/src/task0101.java
@@ -1,7 +1,7 @@
-import java.io.Console;
+//Напиши программу, которая выводит на экран надпись
 
 public class task0101 {
     public static void main(String[] args) {
-
+        System.out.println("Как мне повезло что у меня есть Ира!");
     }
 }
