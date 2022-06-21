e74d289f1b843748d05f1f88b9e4d2a3c297edec
observersilent <denek888@mail.ru>
2022-06-21 20:20:10 +0500

    Добавление задания

---
+++ JavaSyntax/src/Task0102/Solution.java
@@ -0,0 +1,11 @@
+//Написать программу, выводящую на экран надпись "Элли самая красивая" пять раз
+
+package Task0102;
+
+public class Solution {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            System.out.println("Ира самая красивая");
+        }
+    }
+}
